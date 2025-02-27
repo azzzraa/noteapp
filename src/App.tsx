40d637bc4567d8e00f058b9f70b2719010b95f62
Azra <azracatovic13@gmail.com>
2025-02-27 22:37:30 +0100

    Initial commit - Added note app project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { updateConditionalTypeNode } from 'typescript';
 
-function App() {
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+};
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    { id: 1, title: 'note title 1', content: 'content 1' },
+    { id: 2, title: 'note title 2', content: 'content 2' },
+    { id: 3, title: 'note title 3', content: 'content 3' },
+    { id: 4, title: 'note title 4', content: 'content 4' },
+  ]);
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const [selectedNote, setSelectedNote] =
+  useState<Note | null> (null);
+
+  const handleNoteClick = (note:Note) => {
+    setSelectedNote(note);
+    setTitle(note.title)
+    setContent(note.content);
+  }
+
+  const handleAddNote = (event: React.FormEvent) => {
+    event.preventDefault();
+   
+    
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content,
+    };
+
+    setNotes([newNote, ...notes]);
+    setTitle('');
+    setContent('');
+  };
+
+  const handleUpdateNote = (
+    event: React.FormEvent
+  ) => {
+    event.preventDefault();
+    if(!selectedNote) {
+      return;
+    }
+
+    const updateNote: Note = {
+      id: selectedNote.id,
+      title: title,
+      content: content,
+    }
+
+    const updateNotesList = notes.map((note) =>
+      note.id === selectedNote.id
+    ? updateNote 
+    : note
+  )
+
+  setNotes(updateNotesList)
+  setTitle("")
+    setContent("")
+    setSelectedNote(null);
+
+  };
+
+  const handleCancel = ( )=> {
+    setTitle("")
+    setContent("")
+    setSelectedNote(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form" 
+       onSubmit= {(event) => 
+       selectedNote
+       ? handleUpdateNote(event)
+       : handleAddNote(event)}>
+        <input
+          value={title}
+          onChange={(event) =>
+          setTitle(event.target.value)}
+          placeholder="Title"
+          required
+        />
+        <textarea
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+          placeholder="Content"
+          rows={10}
+          required>
+          
+        </textarea>
+
+        {selectedNote ? (
+          <div className='edit-buttons'>
+            <button type ="submit"></button>
+            <button onClick={handleCancel}>Cancel</button>
+          </div>
+        ): (
+        
+      
+        <button type="submit">Add Note</button>
+        )}
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) => (
+          <div className="note-item" onClick={()=> handleNoteClick(note)}>
+            <div className="note-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
